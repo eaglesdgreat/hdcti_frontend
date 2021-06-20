484c1e750867bdc2cbebbbe4bbe3a60949108227
emmanuel <okponobiemmanuel@gmail.com>
2021-06-20 18:11:56 +0100

    added default export to reset password page

---
+++ pages/user_management/reset_password.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+  Hidden,
+  CircularProgress,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import useSWR from "swr";
+import ReactLoading from "react-loading";
+import { useRouter } from "next/router";
+import { PieChart } from "react-minimal-pie-chart";
+
+// import { useStateValue } from '../../StateProviders';
+import TableLayout from "./../Components/Tables";
+import Layout from "./../Components/Layout";
+// import { isAuthenticated } from "./../../lib/auth.helper";
+// import PrivateRoute from "./../../Components/PrivateRoute";
+
+
+
+export default function Home() {
+    const path = '/reset_password'
+
+    return (
+      // <div>Hello World</div>
+      <Layout path={path}></Layout>
+    );
+}
